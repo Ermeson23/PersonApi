51746a362478c4fdfdbf400a7313b928f1025e36
Ermeson23 <joseenem35@gmail.com>
2025-07-30 22:18:01 -0300

    feat: add PersonModels for api

--- Routes/PersonRoute.cs
+++ Routes/PersonRoute.cs
@@ -1,9 +1,11 @@
+using PersonApi.Models;
+
 namespace PersonApi.Routes;
 
 public static class PersonRoute
 {
     public static void PersonRoutes(this WebApplication app)
     {
-        app.MapGet("person", () => "Hello, World");
+        app.MapGet("person", () => new PersonModel("Ermeson"));
     }
 }
