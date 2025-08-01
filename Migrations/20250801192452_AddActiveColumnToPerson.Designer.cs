96d5caf85e4c18112f710fc2a583916416d830c7
Ermeson23 <joseenem35@gmail.com>
2025-08-01 16:46:30 -0300

    fix: add a new migration with active column to person

--- Migrations/20250801192452_AddActiveColumnToPerson.Designer.cs
+++ Migrations/20250801192452_AddActiveColumnToPerson.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PersonApi.Data;
 
@@ -10,9 +11,11 @@ using PersonApi.Data;
 namespace PersonApi.Migrations
 {
     [DbContext(typeof(PersonContext))]
-    partial class PersonContextModelSnapshot : ModelSnapshot
+    [Migration("20250801192452_AddActiveColumnToPerson")]
+    partial class AddActiveColumnToPerson
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "9.0.7");
